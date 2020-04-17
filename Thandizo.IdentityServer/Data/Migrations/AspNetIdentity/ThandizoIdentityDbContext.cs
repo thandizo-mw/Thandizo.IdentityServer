61bc1b2a581f1076c25f7bb6c40b0dfd6b894c3b
Noble Hara <haranoble@gmail.com>
2020-04-17 17:49:45 +0200

    Added AspNetIdentity DB context and migrations

---
+++ Thandizo.IdentityServer/Data/Migrations/AspNetIdentity/ThandizoIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Thandizo.IdentityServer.Models;
+
+namespace Thandizo.IdentityServer.Data.Migrations.AspNetIdentity
+{
+    public class ThandizoIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ThandizoIdentityDbContext(DbContextOptions<ThandizoIdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
