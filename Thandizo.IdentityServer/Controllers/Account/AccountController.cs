e3e1fd5cde6e1cc35a089d336dfd60087e9b615b
Noble Hara <haranoble@gmail.com>
2020-04-21 09:48:38 +0200

    Fixed error consent denied bug

--- Thandizo.IdentityServer/Controllers/Account/AccountController.cs
+++ Thandizo.IdentityServer/Controllers/Account/AccountController.cs
@@ -69,33 +69,6 @@ namespace IdentityServer
             // check if we are in the context of an authorization request
             var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
 
-            // the user clicked the "cancel" button
-            if (button != "login")
-            {
-                if (context != null)
-                {
-                    // if the user cancels, send a result back into IdentityServer as if they 
-                    // denied the consent (even if this client does not require consent).
-                    // this will send back an access denied OIDC error response to the client.
-                    await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
-
-                    // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                    if (await _clientStore.IsPkceClientAsync(context.ClientId))
-                    {
-                        // if the client is PKCE then we assume it's native, so this change in how to
-                        // return the response is for better UX for the end user.
-                        return View("Redirect", new RedirectViewModel { RedirectUrl = model.ReturnUrl });
-                    }
-
-                    return Redirect(model.ReturnUrl);
-                }
-                else
-                {
-                    // since we don't have a valid context, then we just go back to the home page
-                    return Redirect("~/");
-                }
-            }
-
             if (ModelState.IsValid)
             {
                 var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberLogin, lockoutOnFailure: true);
@@ -146,7 +119,7 @@ namespace IdentityServer
         /// <summary>
         /// Show logout page
         /// </summary>
-        [HttpGet]
+        [HttpGet] 
         public async Task<IActionResult> Logout(string logoutId)
         {
             // build a model so the logout page knows what to display
