ebc2a269dbdc40db40d87896eeebac6e6931ecc3
Noble Hara <haranoble@gmail.com>
2020-04-22 09:07:30 +0200

    Added UserManagement service for handling user creation and password updates

---
+++ Thandizo.IdentityServer/Services/UserManagementService/IUserManagementService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Thandizo.DataModels.General;
+using Thandizo.DataModels.Identity.DataTransfer;
+
+namespace Thandizo.IdentityServer.Services
+{
+    public interface IUserManagementService
+    {
+        Task<OutputResponse> RegisterUserAsync(UserDTO userDTO);
+        Task<OutputResponse> UpdatePasswordAsync(PasswordResetDTO passwordResetDTO);
+    }
+}
